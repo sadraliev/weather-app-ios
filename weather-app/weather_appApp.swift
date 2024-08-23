d4a831a056158a8f9a8d210ca237c878d58c4b20
Aibek Sadraliev <a.sadraliev@gmail.com>
2024-08-23 19:27:51 -0400

    Initial Commit

---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Aibek Sadraliev on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
